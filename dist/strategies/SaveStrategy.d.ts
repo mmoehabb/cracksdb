89dab66ecdf1de5e02ca20cb073accf466aca37e
Mahmoud-Ehab <mo.ehab150@gmail.com>
2024-07-08 02:24:25 +0300

    package renamed to cracksdb

--- dist/strategies/SaveStrategy.d.ts
+++
@@ -1,9 +0,0 @@
-import { StateFileContainer } from "../StateFileContainer";
-export declare class SaveStrategy<DataUnit> {
-    private sfc;
-    constructor(sfc: StateFileContainer<DataUnit>);
-    seal(newCrackData?: Array<DataUnit>): void;
-    split(): void;
-    save(): void;
-    saveCrack(index: number): void;
-}
