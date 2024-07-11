d6b631fb546d4b3f5efb50c301257e181e91259e
Mahmoud-Ehab <mo.ehab150@gmail.com>
2024-07-11 18:26:26 +0300

    StateManager seperated from index file.

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,11 @@
 import { FileManager } from "./FileManager";
 import { StateFileFactory } from "./StateFileFactory";
 import { StateFile } from "./StateFileInterface";
+import { StateManager } from "./StateManager";
 
 export {
     FileManager,
     StateFile,
-    StateFileFactory
+    StateFileFactory,
+    StateManager
 };
-
-export class StateManager {
-    private statefiles: object;
-    private passkeys: object;
-
-    private rootdir: string;
-    private fileManager: FileManager;
-    private factory: StateFileFactory;
-
-    constructor(rootdir: string, fileManager: FileManager) {
-        this.statefiles = {};
-        this.passkeys = {};
-        this.rootdir = rootdir;
-        this.fileManager = fileManager;
-        this.factory = new StateFileFactory(this.fileManager);
-        fileManager.createDir(rootdir);
-
-        // load existing substates if any
-        const substates = fileManager.exploreDir(rootdir);
-        for (let substate of substates) {
-            const sf = this.add(substate);
-            this.passkeys[substate] = sf.passkey();
-        }
-    }
-
-    get substates() {
-        return Object.keys(this.statefiles);
-    }
-
-    add<DataUnit extends {}>(substate: string, passkey?: string) {
-        if (this.substates.includes(substate))
-            throw Error(`StateManager: add: ${substate} statefile already exists!`);
-        
-        this.fileManager.createDir(`${this.rootdir}/${substate}`);
-        const stateFile = this.factory.create<DataUnit>(this.rootdir, substate, passkey);
-        this.statefiles[substate] = stateFile;
-        if (passkey)
-            this.passkeys[substate] = passkey;
-        return stateFile;
-    }
-
-    get(substate: string): StateFile<object> {
-        if (!this.substates.includes(substate))
-            throw Error(`StateManager: get: ${substate} not found. Make sure you've added it.`);
-        return this.statefiles[substate];
-    }
-
-    remove(substate: string) {
-        if (!this.substates.includes(substate))
-            throw Error(`StateManager: get: ${substate} not found. Make sure you've added it.`);
-        delete this.statefiles[substate];
-        delete this.passkeys[substate];
-    }
-
-    delete(substate: string, passkey: string) {
-        if (!this.passkeys[substate]) 
-            return;
-
-        if (this.passkeys[substate] === passkey) {
-            this.fileManager.deleteDir(`${this.rootdir}/${substate}`)
-        }
-
-        throw Error("StateManager: delete: invalid passkey.");
-    }
-}
