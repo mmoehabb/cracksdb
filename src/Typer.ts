6af5b47d32d8f0b59bb1a697763cc56e49ded340
Mahmoud-Ehab <mo.ehab150@gmail.com>
2024-08-13 23:34:55 +0300

    ARRAY type has been implemented. And defaultOf method that return the default values of different types

--- src/Typer.ts
+++ src/Typer.ts
@@ -1,4 +1,6 @@
+import { argv0 } from "process";
 import { Type } from "./types";
+import { basename } from "path";
 
 export class Typer {
     private types = {
@@ -6,6 +8,7 @@ export class Typer {
         NUMBER: "number",
         BOOLEAN: "boolean",
         OBJECT: "object",
+        ARRAY: "array"
     }
 
     generateType(obj: object): Type {
@@ -14,6 +17,11 @@ export class Typer {
             const type = this.typeOf(obj[key]);
             if (type === this.types.OBJECT)
                 gentype[key] = this.generateType(obj[key])
+            if (type === this.types.ARRAY)
+                gentype[key] = {
+                  length: "number",
+                  ...this.generateType({...obj[key][0]})
+                }
             else if (type === "")
                 continue;
             else 
@@ -23,10 +31,22 @@ export class Typer {
     }
 
     combineTypes(t1: Type, t2: Type) {
-        return {
-            ...t1,
-            ...t2
+      const combined = {...t1, ...t2}  
+      for (let key in t2) {
+        if (t1[key] === undefined) {
+          continue
         }
+        else if (this.typeOf(t1[key]) == this.types.OBJECT) {
+          combined[key] = this.combineTypes(t1[key] as Type, t2[key] as Type)
+        }
+        else if (this.typeOf(t1[key]) == this.types.ARRAY) {
+          combined[key] = this.combineTypes(t1[key] as Type, t2[key] as Type)
+        }
+        else if (t1[key] !== t2[key]) {
+          throw Error("Typer Error: cannot combine two types with similar (string, number, or boolean) fields names but different types.")
+        }
+      }
+      return combined
     }
 
     checkObj(obj: object, type: Type): boolean {
@@ -39,34 +59,70 @@ export class Typer {
             if (t1[key] === undefined)
                 return false;
 
-            if (this.typeOf(t2[key]) !== this.typeOf(t1[key]))
+            const t2_key_type = this.typeOf(t2[key]) // just minor optimization
+            if (t2_key_type !== this.typeOf(t1[key])) {
                 return false;
-
-            if (this.typeOf(t2[key]) === this.types.OBJECT) {
+            }
+            if (t2_key_type === this.types.OBJECT) {
                 if(!this.checkType(t1[key] as Type, t2[key] as Type)) {
                   return false;
                 }
                 continue
             }
-
-            if (t1[key] !== t2[key])
+            if (t2_key_type === this.types.ARRAY) {
+              if(!this.checkType(t1[key] as Type, t2[key] as Type)) {
                 return false;
+              }
+              continue
+            }
+            if (t1[key] !== t2[key]) {
+                return false;
+            }
         }
         return true;
     }
 
     typeOf(val: any) {
         const tmp = typeof val;
-
         if (tmp === "string")
             return this.types.STRING;
         else if (tmp === "number" || tmp === "bigint")
             return this.types.NUMBER;
         else if (tmp === "boolean")
             return this.types.BOOLEAN;
+        else if (tmp === "object" && val?.length !== undefined)
+            return this.types.ARRAY;
         else if (tmp === "object" && tmp !== null)
             return this.types.OBJECT;
+        else
+            throw Error("Typer Error: unknown value type!")
+    }
 
-        return "";
+    defaultOf(type: Type, key: string) {
+      const t = this.typeOf(type[key])
+      if (t == this.types.STRING) {
+        return ""
+      }
+      if (t == this.types.NUMBER) {
+        return 0
+      }
+      if (t == this.types.BOOLEAN) {
+        return false
+      }
+      if (t == this.types.OBJECT) {
+        const obj = {}
+        for (let k in type[key] as Type) {
+          obj[k] = this.defaultOf(type[key] as Type, k)
+        }
+        return obj
+      }
+      if (t == this.types.ARRAY) {
+        const elm = {}
+        for (let k in type[key] as Type) {
+          if (k == "length") continue
+          elm[key] = this.defaultOf(type[key] as Type, k)
+        }
+        return [elm]
+      }
     }
 }
